f82d3b54374eccafd032eac92ff56022554e29bd
Aamirpal <aamirpal@ucreate.co.in>
2021-06-07 20:10:39 +0530

    Update mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,24 +18,9 @@ const { hmrOptions, devServer } = require('./webpack.fix');
 mix.extract();
 
 mix
-  .js('resources/js/app.js', 'public/js')
-  .react()
-  .postCss('resources/css/app.css', 'public/css/app.css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer')
-  ])
+  .react('resources/js/app.js', 'public/js')
+  .babel('public/js/app.js', 'public/js/app-compiled.js')
   .options({
-    hmrOptions: hmrOptions
+    compact: false,
   })
-  .webpackConfig({
-    output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
-    resolve: {
-      alias: {
-        '@': path.resolve('resources/js')
-      }
-    },
-    devServer: devServer
-  })
-  .version()
-  .sourceMaps();
+  .sourceMaps(true, 'source-map');
